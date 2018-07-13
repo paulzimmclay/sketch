b1f1afaa5d979b7c9f0b298d3733201e5dda96b8
pzc <paul.zimmerman.clayton@gmail.com>
2018-07-13 10:59:16 -0500

    finished character_input exercise

--- character_input/character_input.py
+++ character_input/character_input.py
@@ -1,3 +1,13 @@
+import datetime
+
 user_name = input('What\'s your name? ')
 user_age = input('What\'s your age? ')
-print(f'{user_name} is {user_age}')
+message_print_number = input('How many times do you want to know it? ')
+
+current_time = datetime.datetime.now()
+
+year_to_turn_100 = int(current_time.year) - int(user_age) + 100
+
+# Need user name and years until user turns 100.
+for i in range(0, int(message_print_number)):
+  print(f'{user_name} will turn 100 in the year {year_to_turn_100}')
