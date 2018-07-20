433a54901169a6b9c59cff58a45e62e58b0f1981
pzc <paul.zimmerman.clayton@gmail.com>
2018-07-20 11:16:23 -0500

    naming list exercise

---
+++ name_lists_exercise/nle.py
@@ -0,0 +1,24 @@
+name_list = [
+  {'name':'Katie'},
+  # {'name':'Paul'}
+  # {'name':'James'},
+  # {'name':'Pen Pen'}
+  ]
+
+# Get list of names like this: ['Katie', 'Paul', 'James', 'Pen Pen']
+def namelist(name_list):
+  just_names = []
+  for name_dict in name_list:
+    just_names.append(name_dict['name'])
+
+  if len(just_names) == 1:
+    # return f'{just_names[0]}'
+    return ('%s' %just_names[0])
+  elif len(just_names) == 2:
+    return f'{" & ".join(just_names)}'
+  elif len(just_names) >= 2:
+    return f'{", ".join(just_names[0:-1])} & {just_names[-1]}'
+  # else len(just_names) < 1:
+  #   return ''
+
+print(namelist(name_list))
